7569cdaabf58e984c4d3bda57563fda1404d1669
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2025-09-13 21:21:23 +0000

    copia simples em outro arquivo

--- analise_id.c
+++ analise_id.c
@@ -3,16 +3,33 @@
 
 int main() {
     FILE *arquivo;
-    arquivo = fopen("sort.txt", "r");
+    FILE *tokens;
     int ch;
+
+    arquivo = fopen("sort.txt", "r");
+    
 
     if (arquivo == NULL) {
         perror("Erro ao abrir o arquivo 'sort.txt'");
         return 1;
     }
 
+    tokens = fopen("tokens.txt", "w");
+    if (tokens == NULL) {
+        perror("Erro ao criar o arquivo de destino (copia.txt)");
+        fclose(arquivo);
+        return 1;
+    }
+
     while((ch = fgetc(arquivo)) != EOF) {
+
+        fputc(ch, tokens);
+
         printf("%c",ch);
     }
+
+    fclose(arquivo);
+    fclose(tokens);
+
     return 0;
 }
