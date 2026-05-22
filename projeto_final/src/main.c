9f9fb824da9736687408fafe1c959b75697c9582
joaopaulorocha052 <joaopaulorocha052@gmail.com>
2026-05-22 09:55:52 -0300

    [feat] Adding quad list to main

--- projeto_final/src/main.c
+++ projeto_final/src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "code_gen.h"
 #include "symbol_table.h"
+#include "utils.h"
 #include "../get_opt/options.h"
 
 
@@ -13,15 +14,18 @@ extern HashTable* table;
 extern char* temp_name_buffer;
 extern char* token_string;
 extern int lineno;
+extern struct QuadrupleList* quadList;
 
 int flex_flag;
 
 struct ParseTree* (*parse_function)(void);
 struct ParseTree* sintTree;
 
+
 int main(int argc, char *argv[]){
     
     FILE * file;
+    quadList = init_list();
 
     if(argc < 2) {
         printf("Uso do programa: %s <arquivo_de_leitura> <-l -> uso do flex>\n", argv[0]);
@@ -40,6 +44,9 @@ int main(int argc, char *argv[]){
 
 
     gen_code(sintTree);
+    
+    print_list(quadList);
+
     fclose(file);
     free(temp_name_buffer);
     free(token_string);
